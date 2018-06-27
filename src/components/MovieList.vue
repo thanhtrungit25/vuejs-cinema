daa1adc2837de7e744b7a89ef8913e67b75adec5
TrungDT <thanhtrung.it25@gmail.com>
2018-06-27 15:08:05 +0700

    Displaying session times

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="movie-list">
     <div v-if="filteredMovies.length">
-      <movie-item v-for="(movie, index) in filteredMovies" :key="index" v-bind:movie="movie.movie"></movie-item>
+      <movie-item 
+        v-for="(movie, index) in filteredMovies" 
+        :key="index" 
+        v-bind:movie="movie.movie"
+        v-bind:sessions="movie.sessions"
+      >
+      </movie-item>
     </div>
     <div v-else-if="movies.length" class="no-results">
       No Results.
