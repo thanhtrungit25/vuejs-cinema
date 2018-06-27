c8ceb8bca80dcc550f277d3d22bb942884aa8307
TrungDT <thanhtrung.it25@gmail.com>
2018-06-27 10:08:30 +0700

    Refactor components to SFCs

---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="movie-list">
+    <div v-for="(movie, index) in filteredMovies" :key="index" class="movie">{{ movie.title }}</div>
+  </div>
+</template>
+<script>
+import genres from '../util/genres';
+
+export default {
+  props: ['genre', 'time'],
+  data: function() {
+    return {
+      movies: [
+        { title: 'Kill Bill', genre: genres.CRIME },
+        { title: 'Home Alone', genre: genres.COMEDY },
+        { title: 'Austin Powers', genre: genres.COMEDY },
+      ],
+    };
+  },
+  methods: {
+    moviePassesGenderFilter(movie) {
+      if (!this.genre.length) {
+        return true;
+      } else {
+        return this.genre.find(genre => movie.genre === genre);
+      }
+    },
+  },
+  computed: {
+    filteredMovies() {
+      return this.movies.filter(this.moviePassesGenderFilter);
+    },
+  },
+};
+</script>
